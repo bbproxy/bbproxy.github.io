6fb1ea6da37981c520b721fd3faf3a4fe32e031c
BannerBomb <BannerBomb55@gmail.com>
2016-09-18 00:25:37 -0400

    Delete allParameter.java

--- home287/allParameter.java
+++
@@ -1,8 +0,0 @@
-Map<String, String[]> parameters = request.getParameterMap();
-for(String parameter : parameters.keySet()) {
-	if(!parameter.toLowerCase().startsWith("submitbutton")) {
-	        String[] values = parameters.get(parameter);
-	        //your code here
-	        %> <p> <%= parameter %>: <strong><%= values[0] %></strong> </p> <%
-	}
-}
