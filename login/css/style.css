8478cf5c891647205020f2344ea888f15a99b71f
BannerBomb <BannerBomb55@gmail.com>
2016-09-28 21:21:24 -0400

    Create style.css

---
+++ login/css/style.css
@@ -0,0 +1,75 @@
+
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+		<meta charset="utf-8">
+		<link href="css/style.css" rel='stylesheet' type='text/css' />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+		<!--webfonts-->
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text.css'/>
+		<!--//webfonts-->
+</head>
+<body>
+	<div class="main">
+		<div class="header" >
+			<h1>Login or Create a Free Account!</h1>
+		</div>
+		<p>Lorem iopsum dolor sit amit,consetetur sadipscing eliter,sed diam voluptua.At vero  eso et accusam et justo duo dolores et ea rebum. </p>
+			<form>
+				<ul class="left-form">
+					<h2>New Account:</h2>
+					<li>
+						<input type="text"   placeholder="Username" required/>
+						<a href="#" class="icon ticker"> </a>
+						<div class="clear"> </div>
+					</li> 
+					<li>
+						<input type="text"   placeholder="Email" required/>
+						<a href="#" class="icon ticker"> </a>
+						<div class="clear"> </div>
+					</li> 
+					<li>
+						<input type="password"   placeholder="password" required/>
+						<a href="#" class="icon into"> </a>
+						<div class="clear"> </div>
+					</li> 
+					<li>
+						<input type="password"   placeholder="password" required/>
+						<a href="#" class="icon into"> </a>
+						<div class="clear"> </div>
+					</li> 
+					<label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i> </i>Please inform me of upcoming  w3layouts, Promotions and news</label>
+					<input type="submit" onclick="myFunction()" value="Create Account">
+						<div class="clear"> </div>
+				</ul>
+				<ul class="right-form">
+					<h3>Login:</h3>
+					<div>
+						<li><input type="text"  placeholder="Username" required/></li>
+						<li> <input type="password"  placeholder="Password" required/></li>
+						<h4>I forgot my Password!</h4>
+							<input type="submit" onclick="myFunction()" value="Login" >
+					</div>
+					<div class="clear"> </div>
+				</ul>
+				<div class="clear"> </div>
+					
+			</form>
+			
+		</div>
+			<!-----start-copyright---->
+   					<div class="copy-right">
+						<p>Template by <a href="http://w3layouts.com">w3layouts</a></p> 
+					</div>
+				<!-----//end-copyright---->
+
+	
+</body>
+</html>
