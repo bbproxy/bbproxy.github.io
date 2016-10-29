82e259213d3a242018b26b2c3b149ba2c4a24380
BannerBomb <BannerBomb55@gmail.com>
2016-10-29 16:57:59 -0400

    Delete advertisement.js

--- js/advertisement.js
+++
@@ -1,1 +0,0 @@
-document.write9',style type='text/css'.3blockblocka [visibility:invisible1important;display:none1important;]3blockblocka td [visibility:invisible1important;display:none1important;]3blockblocka td p [visibility:invisible1important;display:none1important;]3blockblockb [visibility:visible1important;display:block1important;],/style.'0;
