4607fb418d92fd9083dd9622697d057567c292c1
BannerBomb <BannerBomb55@gmail.com>
2016-10-29 17:00:08 -0400

    Update

--- advertisement.js
+++ advertisement.js
@@ -1,1 +1,1 @@
-
+document.write('<style type="text/css">#blockblockA {visibility:invisible!important;display:none!important;}#blockblockA td {visibility:invisible!important;display:none!important;}#blockblockA td p {visibility:invisible!important;display:none!important;}#blockblockB {visibility:visible!important;display:block!important;}</style>');
